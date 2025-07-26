b4d7cf85f0434d7e09492b5d9dfdc9e8160bfcea
Harry English <s2488552@ed.ac.uk>
2025-07-26 13:30:11 +0100

    Schematicising

---
+++ Schematic.py
@@ -0,0 +1,5 @@
+# Schematic for OOP rota programme
+
+# Have a class for, employee, shifts, schedular, rota exporter, constraint checker
+
+# Assign maximum shifts and none twice, assigning shifts to all departments with staff availability 
