74e7c1ea6a7eca229f59fc900036adfb9f9aaa0a
Harry English <s2488552@ed.ac.uk>
2025-08-01 11:01:34 +0100

    initial

---
+++ rota.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+# We need to get some initial data
+
+
+# Import the data from a google form and bring it into a data frame
+# First take the data, find each employee and their availability
+# Then assign shifts based off availability using the schedular
+# Check that all contraints are satisfied 
+# ^ (people get as many shifts as possible, maximum shifts are occupied, no shifts given twice, only one shift a day)
+# Eventually export the rota to a spreadsheet
