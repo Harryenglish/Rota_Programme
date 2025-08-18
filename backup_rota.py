f746eaca1b405b8f950a9d6dd0564f232eca5bf4
Harry English <s2488552@ed.ac.uk>
2025-08-18 20:16:56 +0100

    fail

--- backup_rota.py
+++ backup_rota.py
@@ -27,7 +27,7 @@ index_to_label = {
 }
 
 
-df = pd.read_excel("rota data export (Responses).xlsx")
+df = pd.read_excel("test.xlsx")
 
 df.columns.values[1] = 'Name'
 del df[df.columns[0]]
@@ -47,21 +47,26 @@ for col in initial_columns:
 # Now we need a way of classifying every employee and their availability 
 
 class Employee:
-        '''
-        All that is gathered from the form is name and availability. So we will make this our object.
+    def __init__(self, name, availability):
+        self.name = name
+        # availability is a list of tuples: (day, period)
+        # e.g., [('Monday', 'morning'), ('Tuesday', 'evening')]
+        self.availability = availability  
+        self.assigned = []
 
-        The purpose of this class is so we can access availability information as efficiently as possible.
-        '''
-        def __init__(self, employee, availability_flat):
-            self.employee = employee
-            self.availability = {
-            index_to_label[i]: available
-            for i, available in enumerate(availability_flat)
-        } 
-            self.assigned = []
+    def is_available_for(self, day, period):
+        # Check if employee is available for this shift AND hasn't already been assigned today
+        available_today = self.count_assigned_on(day) < 1
+        return (day, period) in self.availability and available_today
 
-        def __repr__(self):
-            return f"Employee(name={self.employee}, availability={self.availability})"    
+    def count_assigned_on(self, day):
+        # Count how many shifts already assigned on this day
+        return sum(1 for shift in self.assigned if shift.day == day)
+
+    def mark_unassigned(self, shift):
+        # Optional: remove shift from assigned if needed
+        if shift in self.assigned:
+            self.assigned.remove(shift)
 
 employees = []
 shift_columns = df.columns[1:]
@@ -92,9 +97,13 @@ class Shift:
         self.department = department
         self.day = day
         self.assigned = []
+        self.unassignable = False
 
     def __repr__(self):
         return f"Shift({self.period}, {self.time_range}, dept={self.department}, assigned={self.assigned})"
+    
+    def status(self):
+        print(self.assigned)
     
 morning_required = ["7:00 - 13:00", "10:00 - 18:00", "8:00 - 13:00", "9:00 - 17:00"]
 afternoon_required = ["16:00 - 00:00", "12:00 - 22:00", "14:00 - 23:00"]
@@ -114,12 +123,13 @@ department_mapping = {
 
 all_shifts = []
 
-for time in morning_required:
-    all_shifts.append(Shift("morning", time, department_mapping[time]))
-for time in afternoon_required:
-    all_shifts.append(Shift("afternoon", time, department_mapping[time]))
-for time in evening_required:
-    all_shifts.append(Shift("evening", time, department_mapping[time]))
+for day in DAYS:
+    for time in morning_required:
+        all_shifts.append(Shift("morning", time, department_mapping[time], day))
+    for time in afternoon_required:
+        all_shifts.append(Shift("afternoon", time, department_mapping[time], day))
+    for time in evening_required:
+        all_shifts.append(Shift("evening", time, department_mapping[time], day))
 
 # print(all_shifts)
 
@@ -146,18 +156,72 @@ class Scheduler:
         self.all_shifts = all_shifts
 
       def Scheduling(self):
-        
-      
-      
-      def Exporter(self, all_assigned):
-          '''
-          This method will be in charge of exporting the rota into a viewable format
+        sorted_shifts = sorted(enumerate(self.all_shifts),key=lambda x: self.count_available(x[1], x[0]))
 
-          Assign shift times to department
-          '''
-          
+        for shift_index, shift in enumerate([s for i, s in sorted_shifts]):
+            candidates = self.get_candidates(shift)
+            if not candidates:
+                shift.unassignable = True
+                continue
+
+            employee = min(candidates, key=lambda e: len(e.assigned))
+            shift.assigned.append(employee)
+            employee.assigned.append(shift)
+
+            for e in candidates:
+                if e != employee:
+                    e.mark_unassigned(shift)
+
+      def get_candidates(self, shift):
+        return [e for e in self.employees if e.is_available_for(shift.day, shift.period)]
+
+      def count_available(self, shift, shift_index):
+        return len(self.get_candidates(shift))
+      
+      
+      
+      
               
         
+# scheduling class need method for heuristics, scheduling from most constrained
+# least assigned employee, no shifts for one person twice in one day
+
+# shifts need attribute that show if nobody can take it
+
+# employee needs attribute that shows if they are available but not been assigned
 
 
 
+
+
+
+
+
+schedule_dict = {}
+
+for shift in all_shifts:
+    day = shift.day
+    dept = shift.department
+    if day not in schedule_dict:
+        schedule_dict[day] = {}
+    if dept not in schedule_dict[day]:
+        schedule_dict[day][dept] = []
+    schedule_dict[day][dept].append(shift)
+
+# Flatten into rows for a DataFrame
+rows = []
+for day, depts in schedule_dict.items():
+    for dept, shifts in depts.items():
+        for shift in shifts:
+            rows.append({
+                "Day": day,
+                "Department": dept,
+                "Period": shift.period,
+                "Time": shift.time_range,
+                "Assigned": ", ".join([e.employee for e in shift.assigned]) or "Unassigned"
+            })
+
+df_schedule = pd.DataFrame(rows)
+
+# Save to Excel
+df_schedule.to_excel("rota_schedule.xlsx", index=False)
